ac92a57bd674803896a3e629898d36acd9be555d
boranpek <boranpekell@hotmail.com>
2021-03-15 15:32:48 +0300

    implement onboarding view

--- lib/presentation/screens/onboarding_view/onboarding_view.dart
+++ lib/presentation/screens/onboarding_view/onboarding_view.dart
@@ -1,5 +1,8 @@
-import 'package:dongu_mobile/presentation/widgets/text/locale_text.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:dongu_mobile/utils/constants/image_constant.dart';
 import 'package:dongu_mobile/utils/extensions/context_extension.dart';
+import 'package:dongu_mobile/utils/extensions/string_extension.dart';
+import 'package:dongu_mobile/utils/locale_keys.g.dart';
 import 'package:dongu_mobile/utils/theme/app_colors/app_colors.dart';
 import 'package:dongu_mobile/utils/theme/app_text_styles/app_text_styles.dart';
 import 'package:flutter/material.dart';
@@ -10,28 +13,47 @@ class OnboardingView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(
-          alignment: Alignment.bottomCenter,
-          decoration: BoxDecoration(
-            color: Colors.white,
-          ),
-          child: Image.asset(
-            "assets/images/onboarding_background.png",
-            fit: BoxFit.cover,
+        Positioned(
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            padding: EdgeInsets.only(top: context.dynamicHeight(0.14)),
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Image.asset(
+              ImageConstant.ONBOARDING_BACKGROUND,
+              fit: BoxFit.fill,
+            ),
           ),
         ),
         Container(
           alignment: Alignment(0.5, -0.2),
           padding: EdgeInsets.symmetric(horizontal: context.dynamicWidht(0.1)),
-          child: LocaleText(
-            alignment: TextAlign.center,
-            text: "Gıda israfıyla savaşmak, gezegenimizi korumak için çıktığımız yolculukta aramıza hoş geldin!",
-            maxLines: 4,
-            style: GoogleFonts.montserrat(
-              decoration: TextDecoration.none,
-              color: AppColors.textColor,
-              fontWeight: FontWeight.w500,
+          child: AutoSizeText.rich(
+            TextSpan(
+              style: AppTextStyles.headlineStyle.copyWith(color: AppColors.textColor),
+              children: [
+                TextSpan(
+                  text: LocaleKeys.onboarding_text1.locale,
+                  style: GoogleFonts.montserrat(
+                    decoration: TextDecoration.none,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: LocaleKeys.onboarding_text2.locale,
+                  style: GoogleFonts.montserrat(
+                    decoration: TextDecoration.none,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
             ),
+            textAlign: TextAlign.center,
+            maxLines: 4,
           ),
         ),
       ],
---
+++ lib/utils/locale_keys.g.dart
@@ -0,0 +1,8 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const onboarding_text1 = 'onboarding.text1';
+  static const onboarding_text2 = 'onboarding.text2';
+  static const onboarding = 'onboarding';
+
+}
