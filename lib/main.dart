02e22613a410b1b77151996d71c427dd4586c0ff
Fatih Can <fatih.can@westerops.com>
2021-03-17 15:49:10 +0300

    fix filter

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,6 @@ class MyApp extends StatelessWidget {
       supportedLocales: context.supportedLocales,
       locale: context.locale,
       onGenerateRoute: _appRouter.onGenerateRoute,
-      home: FilterView(),
     );
 
     /*MultiBlocProvider(
