cb6c5cef2676104433e711d3787a8b11f7ba155c
boranpek <boranpekell@hotmail.com>
2021-03-18 15:22:31 +0300

    add clipped password rules to forgot password view

--- lib/presentation/screens/forgot_password_view/forgot_password_view.dart
+++ lib/presentation/screens/forgot_password_view/forgot_password_view.dart
@@ -1,4 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:dongu_mobile/presentation/screens/register_view/components/clipped_password_rules.dart';
 import 'package:dongu_mobile/presentation/widgets/button/custom_button.dart';
 import 'package:dongu_mobile/presentation/widgets/text/locale_text.dart';
 import 'package:dongu_mobile/utils/constants/image_constant.dart';
@@ -23,61 +24,82 @@ class _ForgotPasswordViewState extends State<ForgotPasswordView> {
   bool enableObscure = true;
   bool isCodeSent = false;
   String dropdownValue = "TR";
+  bool isRulesVisible = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("This will be changed"),
-      ),
-      body: Padding(
-        padding: EdgeInsets.only(
-          top: context.dynamicHeight(0.04),
-          left: context.dynamicWidht(0.06),
-          right: context.dynamicWidht(0.06),
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          if (isRulesVisible) {
+            isRulesVisible = false;
+          }
+        });
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("This will be changed"),
         ),
-        child: Column(
+        body: Stack(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                buildDropDown(context),
-                Container(
-                  height: context.dynamicHeight(0.06),
-                  width: context.dynamicWidht(0.57),
-                  child: buildTextFormField(LocaleKeys.forgot_password_phone.locale, phoneController),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: context.dynamicHeight(0.02),
-            ),
-            Visibility(visible: isCodeSent, child: buildTextFormField(LocaleKeys.forgot_password_activation_code.locale, codeController)),
-            Visibility(
-              visible: isCodeSent,
-              child: SizedBox(
-                height: context.dynamicHeight(0.02),
+            Padding(
+              padding: EdgeInsets.only(
+                top: context.dynamicHeight(0.04),
+                left: context.dynamicWidht(0.06),
+                right: context.dynamicWidht(0.06),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      buildDropDown(context),
+                      Container(
+                        height: context.dynamicHeight(0.06),
+                        width: context.dynamicWidht(0.57),
+                        child: buildTextFormField(LocaleKeys.forgot_password_phone.locale, phoneController),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: context.dynamicHeight(0.02),
+                  ),
+                  Visibility(visible: isCodeSent, child: buildTextFormField(LocaleKeys.forgot_password_activation_code.locale, codeController)),
+                  Visibility(
+                    visible: isCodeSent,
+                    child: SizedBox(
+                      height: context.dynamicHeight(0.02),
+                    ),
+                  ),
+                  Visibility(visible: isCodeSent, child: buildTextFormFieldPassword(LocaleKeys.forgot_password_new_password.locale)),
+                  SizedBox(
+                    height: context.dynamicHeight(0.02),
+                  ),
+                  CustomButton(
+                    onPressed: () {
+                      setState(() {
+                        isCodeSent = true;
+                      });
+                    },
+                    width: double.infinity,
+                    title: isCodeSent ? LocaleKeys.forgot_password_reset_password : LocaleKeys.forgot_password_send_code,
+                    color: isCodeSent ? AppColors.disabledButtonColor : AppColors.greenColor,
+                    borderColor: isCodeSent ? AppColors.disabledButtonColor : AppColors.greenColor,
+                    textColor: Colors.white,
+                  ),
+                  SizedBox(
+                    height: context.dynamicHeight(0.02),
+                  ),
+                  buildVisibilitySendAgainCode,
+                ],
               ),
             ),
-            Visibility(visible: isCodeSent, child: buildTextFormField(LocaleKeys.forgot_password_new_password.locale, passwordController)),
-            SizedBox(
-              height: context.dynamicHeight(0.02),
+            Positioned(
+              top: context.height > 800 ? context.dynamicHeight(0.1) : context.dynamicHeight(0.125),
+              left: context.dynamicWidht(0.365),
+              right: context.dynamicWidht(0.365),
+              child: Visibility(visible: isRulesVisible, child: ClippedPasswordRules(passwordController: passwordController)),
             ),
-            CustomButton(
-              onPressed: () {
-                setState(() {
-                  isCodeSent = true;
-                });
-              },
-              width: double.infinity,
-              title: isCodeSent ? LocaleKeys.forgot_password_reset_password : LocaleKeys.forgot_password_send_code,
-              color: isCodeSent ? AppColors.disabledButtonColor : AppColors.greenColor,
-              borderColor: isCodeSent ? AppColors.disabledButtonColor : AppColors.greenColor,
-              textColor: Colors.white,
-            ),
-            SizedBox(
-              height: context.dynamicHeight(0.02),
-            ),
-            buildVisibilitySendAgainCode,
           ],
         ),
       ),
@@ -144,37 +166,67 @@ class _ForgotPasswordViewState extends State<ForgotPasswordView> {
     );
   }
 
+  TextFormField buildTextFormFieldPassword(String labelText) {
+    return TextFormField(
+      onChanged: (value) {
+        setState(() {
+          isRulesVisible = true;
+        });
+      },
+      controller: passwordController,
+      obscureText: enableObscure,
+      decoration: InputDecoration(
+        suffixIconConstraints: BoxConstraints.tightFor(width: context.dynamicWidht(0.05), height: context.dynamicHeight(0.02)),
+        suffixIcon: GestureDetector(
+          onTap: () {
+            setState(() {
+              enableObscure = !enableObscure;
+            });
+          },
+          child: enableObscure
+              ? SvgPicture.asset(
+                  ImageConstant.REGISTER_LOGIN_OBSCURE_ENABLE_ICON,
+                )
+              : SvgPicture.asset(
+                  ImageConstant.REGISTER_LOGIN_OBSCURE_DISABLE_ICON,
+                ),
+        ),
+        labelText: labelText,
+        prefix: Text(
+          "",
+        ),
+        labelStyle: AppTextStyles.subTitleStyle,
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: AppColors.borderAndDividerColor, width: 2),
+          borderRadius: BorderRadius.circular(4.0),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: AppColors.borderAndDividerColor, width: 2),
+          borderRadius: BorderRadius.circular(4.0),
+        ),
+        border: OutlineInputBorder(
+          borderSide: BorderSide(),
+          borderRadius: BorderRadius.circular(4.0),
+        ),
+      ),
+    );
+  }
+
   TextFormField buildTextFormField(String labelText, TextEditingController controller) {
     return TextFormField(
+      onTap: () {
+        setState(() {
+          isRulesVisible = false;
+        });
+      },
       controller: controller,
-      obscureText: enableObscure && controller == passwordController,
       decoration: InputDecoration(
-        fillColor: Colors.white,
-        filled: true,
+        labelText: labelText,
         prefixText: controller == phoneController
             ? dropdownValue == 'TR'
                 ? "+90"
                 : "+1"
-            : "",
-        suffixIconConstraints:
-            controller == passwordController ? BoxConstraints.tightFor(width: context.dynamicWidht(0.05), height: context.dynamicHeight(0.02)) : null,
-        suffixIcon: controller == passwordController
-            ? GestureDetector(
-                onTap: () {
-                  setState(() {
-                    enableObscure = !enableObscure;
-                  });
-                },
-                child: enableObscure
-                    ? SvgPicture.asset(
-                        ImageConstant.REGISTER_LOGIN_OBSCURE_ENABLE_ICON,
-                      )
-                    : SvgPicture.asset(
-                        ImageConstant.REGISTER_LOGIN_OBSCURE_DISABLE_ICON,
-                      ),
-              )
             : null,
-        labelText: labelText,
         labelStyle: AppTextStyles.subTitleStyle,
         enabledBorder: OutlineInputBorder(
           borderSide: BorderSide(color: AppColors.borderAndDividerColor, width: 2),
