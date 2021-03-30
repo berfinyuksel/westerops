1f5d41d68efa82e59b5d48ce1fff441e0fdd3dcc
boranpek <boranpekell@hotmail.com>
2021-03-30 10:38:13 +0300

    develop cart view

---
+++ lib/presentation/screens/cart_view/cart_view.dart
@@ -0,0 +1,151 @@
+import 'package:dongu_mobile/presentation/screens/past_order_detail_view/components/past_order_detail_basket_list_tile.dart';
+import 'package:dongu_mobile/presentation/screens/past_order_detail_view/components/past_order_detail_body_title.dart';
+import 'package:dongu_mobile/presentation/screens/past_order_detail_view/components/past_order_detail_payment_list_tile.dart';
+import 'package:dongu_mobile/presentation/screens/past_order_detail_view/components/past_order_detail_total_payment_list_tile.dart';
+import 'package:dongu_mobile/presentation/widgets/button/custom_button.dart';
+import 'package:dongu_mobile/presentation/widgets/scaffold/custom_scaffold.dart';
+import 'package:dongu_mobile/presentation/widgets/text/locale_text.dart';
+import 'package:dongu_mobile/utils/constants/image_constant.dart';
+import 'package:dongu_mobile/utils/extensions/context_extension.dart';
+import 'package:dongu_mobile/utils/extensions/string_extension.dart';
+import 'package:dongu_mobile/utils/locale_keys.g.dart';
+import 'package:dongu_mobile/utils/theme/app_colors/app_colors.dart';
+import 'package:dongu_mobile/utils/theme/app_text_styles/app_text_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CartView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      title: "Sepetim",
+      body: Column(
+        children: [
+          ListView(
+            shrinkWrap: true,
+            padding: EdgeInsets.only(
+              top: context.dynamicHeight(0.02),
+            ),
+            children: [
+              PastOrderDetailBodyTitle(
+                title: LocaleKeys.past_order_detail_body_title_1,
+              ),
+              SizedBox(
+                height: context.dynamicHeight(0.01),
+              ),
+              buildRestaurantListTile(context),
+              SizedBox(
+                height: context.dynamicHeight(0.04),
+              ),
+              PastOrderDetailBodyTitle(
+                title: LocaleKeys.past_order_detail_body_title_3,
+              ),
+              SizedBox(
+                height: context.dynamicHeight(0.01),
+              ),
+              PastOrderDetailBasketListTile(
+                title: "Anadolu Lezzetleri",
+                price: 35,
+                withDecimal: false,
+                subTitle: "Pastırmalı Kuru Fasulye,\n1 porsiyon Kornişon Turşu",
+              ),
+              PastOrderDetailBasketListTile(
+                title: "Vegan 4 Paket",
+                price: 35,
+                withDecimal: false,
+                subTitle: "Brokoli Salatası, Vegan Sos\nSoya Soslu Fasulye",
+              ),
+              SizedBox(
+                height: context.dynamicHeight(0.04),
+              ),
+              PastOrderDetailBodyTitle(
+                title: LocaleKeys.past_order_detail_body_title_4,
+              ),
+              SizedBox(
+                height: context.dynamicHeight(0.01),
+              ),
+              PastOrderDetailPaymentListTile(
+                title: LocaleKeys.past_order_detail_payment_1,
+                price: 70,
+                lineTrough: false,
+                withDecimal: false,
+              ),
+              PastOrderDetailPaymentListTile(
+                title: LocaleKeys.past_order_detail_payment_2,
+                price: 4.50,
+                lineTrough: true,
+                withDecimal: true,
+              ),
+                PastOrderDetailPaymentListTile(
+                title: LocaleKeys.past_order_detail_payment_2,
+                price: 4.50,
+                lineTrough: true,
+                withDecimal: true,
+              ),
+                PastOrderDetailPaymentListTile(
+                title: LocaleKeys.past_order_detail_payment_2,
+                price: 4.50,
+                lineTrough: true,
+                withDecimal: true,
+              ),
+                PastOrderDetailPaymentListTile(
+                title: LocaleKeys.past_order_detail_payment_2,
+                price: 4.50,
+                lineTrough: true,
+                withDecimal: true,
+              ),
+              PastOrderDetailPaymentListTile(
+                title: "${LocaleKeys.past_order_detail_payment_3.locale} (2)*",
+                price: 0.50,
+                lineTrough: false,
+                withDecimal: true,
+              ),
+              PastOrderDetailTotalPaymentListTile(
+                title: LocaleKeys.past_order_detail_payment_4,
+                price: 70.50,
+                withDecimal: true,
+              ),
+              SizedBox(
+                height: context.dynamicHeight(0.02),
+              ),
+            ],
+          ),
+          Spacer(),
+          Padding(
+            padding: EdgeInsets.only(
+              left: context.dynamicWidht(0.06),
+              right: context.dynamicWidht(0.06),
+              bottom: context.dynamicHeight(0.02),
+            ),
+            child: CustomButton(
+              width: double.infinity,
+              title: "Devam et",
+              color: AppColors.greenColor,
+              borderColor: AppColors.greenColor,
+              textColor: Colors.white,
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  ListTile buildRestaurantListTile(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.only(
+        left: context.dynamicWidht(0.06),
+        right: context.dynamicWidht(0.06),
+      ),
+      trailing: SvgPicture.asset(
+        ImageConstant.COMMONS_FORWARD_ICON,
+      ),
+      tileColor: Colors.white,
+      title: LocaleText(
+        text: "Canım Büfe",
+        style: AppTextStyles.bodyTextStyle,
+      ),
+      onTap: () {},
+    );
+  }
+}
