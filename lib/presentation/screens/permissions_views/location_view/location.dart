af378c2b40de4859be4a7bed5cd2c6b9a2f442a5
Rabia Yıldız <73601503+rabianyildiz@users.noreply.github.com>
2021-12-06 21:26:25 +0300

    food waste issues

--- lib/presentation/screens/permissions_views/location_view/location.dart
+++ lib/presentation/screens/permissions_views/location_view/location.dart
@@ -93,7 +93,7 @@ class _LocationViewState extends State<LocationView> {
       title: LocaleKeys.premission_location_button1.locale,
       onPressed: () {
         Geolocator.requestPermission();
-        Navigator.pushNamed(context, RouteConstant.CUSTOM_SCAFFOLD);
+        Navigator.pushNamed(context, RouteConstant.FOOD_WASTE_VIEW);
       },
     );
   }
