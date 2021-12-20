8475d79705b776adb8a0e5d4226726bea2ee5437
Rabia Yıldız <73601503+rabianyildiz@users.noreply.github.com>
2021-12-20 12:49:43 +0300

    restaurant detail page some texts fixed

--- lib/presentation/screens/restaurant_details_views/restaurant_detail_view/components/custom_card_and_body.dart
+++ lib/presentation/screens/restaurant_details_views/restaurant_detail_view/components/custom_card_and_body.dart
@@ -163,7 +163,7 @@ class _CustomCardAndBodyState extends State<CustomCardAndBody>
                 restaurantTitleAndAddressColumn(),
                 Spacer(flex: 2),
                 Padding(
-                  padding: EdgeInsets.only(top: context.dynamicHeight(0.009)),
+                  padding: EdgeInsets.only(top: context.dynamicHeight(0.014)),
                   child: restaurantStarIconRating(),
                 ),
                 Spacer(flex: 1),
