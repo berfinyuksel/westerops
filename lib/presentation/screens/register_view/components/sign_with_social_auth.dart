ddd281f12ba0c7e0a3afe3239ae57b75f82bcc42
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-03-17 16:37:59 +0300

    login screen bug fixes / new social buttons

--- lib/presentation/screens/register_view/components/sign_with_social_auth.dart
+++ lib/presentation/screens/register_view/components/sign_with_social_auth.dart
@@ -1,45 +1,45 @@
+import 'package:dongu_mobile/presentation/widgets/text/locale_text.dart';
+import 'package:dongu_mobile/utils/theme/app_text_styles/app_text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 
-import '../../../../utils/extensions/context_extension.dart';
-import '../../../../utils/locale_keys.g.dart';
 import '../../../../utils/theme/app_colors/app_colors.dart';
-import '../../../../utils/theme/app_text_styles/app_text_styles.dart';
-import '../../../widgets/text/locale_text.dart';
 
 class SignWithSocialAuth extends StatelessWidget {
   final String? image;
-  final bool? isApple;
+  final String text;
   const SignWithSocialAuth({
     Key? key,
     this.image,
-    this.isApple = false,
+    required this.text,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 40.h,
+      height: 44.h,
+      width: 300.w,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20.0),
+        borderRadius: BorderRadius.circular(40.0),
         color: Colors.white,
         border: Border.all(width: 2.0, color: AppColors.borderAndDividerColor),
       ),
-      child: Row(
+      child: Stack(
+        alignment: Alignment.center,
         children: [
-          SvgPicture.asset(
-            image!,
-            height: 39.h,
+          Positioned(
+            left: 0,
+            child: SvgPicture.asset(
+              image!,
+              height: 40.h,
+            ),
           ),
-          SizedBox(width: 5.w),
           LocaleText(
-            text: isApple!
-                ? LocaleKeys.register_social_auth_apple
-                : LocaleKeys.register_social_auth,
+            text: text,
+            alignment: TextAlign.center,
             style: AppTextStyles.bodyTextStyle.copyWith(fontSize: 10),
           ),
-          SizedBox(width: 10.w),
         ],
       ),
     );
