b8d93dc7249ae6bd4e0c5c7f4573dadbdaa879bb
Boran Pekel <boran.pekel@kobil.com>
2022-04-08 11:07:26 +0300

    refactor code

--- lib/presentation/screens/food_waste_views/food_waste_expanded_view.dart
+++ lib/presentation/screens/food_waste_views/food_waste_expanded_view.dart
@@ -19,208 +19,201 @@ class FoodWasteExpandedView extends StatelessWidget {
     return CustomScaffold(
       isNavBar: false,
       title: LocaleKeys.food_waste_title,
-      body: Container(
+      body: ListView(
         padding: EdgeInsets.only(
-          top: 0.h,
+          top: 26.h,
           left: 45.w,
           right: 45.w,
         ),
-        child: ListView(
-          children: [
-            SizedBox(height: 26.h),
-            LocaleText(
-              text: LocaleKeys.food_waste_expanded_headline1,
-              style: AppTextStyles.headlineStyle,
-              alignment: TextAlign.center,
-              maxLines: 2,
-            ),
-            SizedBox(
-              height: 23.h,
-            ),
-            LocaleText(
-              text: LocaleKeys.food_waste_expanded_body1,
-              style: AppTextStyles.bodyBoldTextStyle,
-              alignment: TextAlign.center,
-              maxLines: 2,
-            ),
-            SizedBox(
-              height: 40.h,
-            ),
-            SvgPicture.asset(
-              ImageConstant.FOOD_WASTE_EXPANDED_IMAGE_1,
-              height: 85.81.h,
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            LocaleText(
-              text: LocaleKeys.food_waste_expanded_body2,
-              style: AppTextStyles.bodyBoldTextStyle,
-              alignment: TextAlign.center,
-            ),
-            SizedBox(
-              height: 40.h,
-            ),
-            SvgPicture.asset(
-              ImageConstant.FOOD_WASTE_EXPANDED_IMAGE_2,
-              height: 137.02.h,
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            LocaleText(
-              text: LocaleKeys.food_waste_expanded_body3,
-              style: AppTextStyles.bodyBoldTextStyle,
-              alignment: TextAlign.center,
-            ),
-            SizedBox(
-              height: 50.2.h,
-            ),
-            LocaleText(
-              text: LocaleKeys.food_waste_expanded_headline2,
-              style: AppTextStyles.headlineStyle,
-              alignment: TextAlign.center,
-              maxLines: 1,
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            Center(
-              child: Stack(children: [
-                SvgPicture.asset(
-                  ImageConstant.FOOD_WASTE_EXPANDED_IMAGE_3,
-                  height: 145.h,
-                ),
-                Positioned(
-                  left: 0.w,
-                  right: 0.w,
-                  top: 40.h,
-                  child: Center(
-                    child: Expanded(
-                      child: AutoSizeText.rich(
-                        TextSpan(children: [
-                          TextSpan(
-                            text: "%33'ü\n",
-                            style: AppTextStyles.bodyBoldTextStyle.copyWith(
-                                fontSize: 32.sp,
-                                fontWeight: FontWeight.w700,
-                                color: Colors.black),
-                          ),
-                          TextSpan(
-                            text: "\t\t\t\t\t\t\tİsraf",
-                            style: GoogleFonts.montserrat(
-                              textStyle: TextStyle(color: Colors.black),
-                              fontSize: 17.sp,
-                              fontWeight: FontWeight.w300,
-                              fontStyle: FontStyle.normal,
-                            ),
-                          ),
-                        ]),
+        children: [
+          LocaleText(
+            text: LocaleKeys.food_waste_expanded_headline1,
+            style: AppTextStyles.headlineStyle,
+            alignment: TextAlign.center,
+            maxLines: 2,
+          ),
+          SizedBox(
+            height: 23.h,
+          ),
+          LocaleText(
+            text: LocaleKeys.food_waste_expanded_body1,
+            style: AppTextStyles.bodyBoldTextStyle,
+            alignment: TextAlign.center,
+            maxLines: 2,
+          ),
+          SizedBox(
+            height: 40.h,
+          ),
+          SvgPicture.asset(
+            ImageConstant.FOOD_WASTE_EXPANDED_IMAGE_1,
+            height: 85.81.h,
+          ),
+          SizedBox(
+            height: 20.h,
+          ),
+          LocaleText(
+            text: LocaleKeys.food_waste_expanded_body2,
+            style: AppTextStyles.bodyBoldTextStyle,
+            alignment: TextAlign.center,
+          ),
+          SizedBox(
+            height: 40.h,
+          ),
+          SvgPicture.asset(
+            ImageConstant.FOOD_WASTE_EXPANDED_IMAGE_2,
+            height: 137.02.h,
+          ),
+          SizedBox(
+            height: 20.h,
+          ),
+          LocaleText(
+            text: LocaleKeys.food_waste_expanded_body3,
+            style: AppTextStyles.bodyBoldTextStyle,
+            alignment: TextAlign.center,
+          ),
+          SizedBox(
+            height: 50.2.h,
+          ),
+          LocaleText(
+            text: LocaleKeys.food_waste_expanded_headline2,
+            style: AppTextStyles.headlineStyle,
+            alignment: TextAlign.center,
+            maxLines: 1,
+          ),
+          SizedBox(
+            height: 20.h,
+          ),
+          Center(
+            child: Stack(children: [
+              SvgPicture.asset(
+                ImageConstant.FOOD_WASTE_EXPANDED_IMAGE_3,
+                height: 145.h,
+              ),
+              Positioned(
+                left: 0,
+                right: 0,
+                top: 40.h,
+                child: Center(
+                  child: AutoSizeText.rich(
+                    TextSpan(children: [
+                      TextSpan(
+                        text: "%33'ü\n",
+                        style: AppTextStyles.bodyBoldTextStyle.copyWith(fontSize: 32.sp, fontWeight: FontWeight.w700, color: Colors.black),
                       ),
-                    ),
+                      TextSpan(
+                        text: "İsraf",
+                        style: GoogleFonts.montserrat(
+                          textStyle: TextStyle(color: Colors.black),
+                          fontSize: 17.sp,
+                          fontWeight: FontWeight.w300,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                    ]),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-              ]),
-            ),
-            SizedBox(
-              height: 10.h,
-            ),
-            LocaleText(
-              text: LocaleKeys.food_waste_expanded_body4,
-              style: AppTextStyles.bodyBoldTextStyle,
-              alignment: TextAlign.center,
-              maxLines: 2,
-            ),
-            SizedBox(
-              height: 40.h,
-            ),
-            SvgPicture.asset(
-              ImageConstant.FOOD_WASTE_EXPANDED_IMAGE_4,
-              height: 145.h,
-            ),
-            SizedBox(
-              height: 10.h,
-            ),
-            LocaleText(
-              text: LocaleKeys.food_waste_expanded_body5,
-              style: AppTextStyles.bodyBoldTextStyle,
-              alignment: TextAlign.center,
-              maxLines: 2,
-            ),
-            SizedBox(
-              height: 40.h,
-            ),
-            SvgPicture.asset(
-              ImageConstant.FOOD_WASTE_EXPANDED_IMAGE_5,
-              height: 145.h,
-            ),
-            SizedBox(
-              height: 10.h,
-            ),
-            LocaleText(
-              text: LocaleKeys.food_waste_expanded_body6,
-              style: AppTextStyles.bodyBoldTextStyle,
-              alignment: TextAlign.center,
-              maxLines: 2,
-            ),
-            SizedBox(
-              height: 40.h,
-            ),
-            SvgPicture.asset(
-              ImageConstant.FOOD_WASTE_EXPANDED_IMAGE_6,
-              height: 145.h,
-            ),
-            SizedBox(
-              height: 10.h,
-            ),
-            LocaleText(
-              text: LocaleKeys.food_waste_expanded_body7,
-              style: AppTextStyles.bodyBoldTextStyle,
-              alignment: TextAlign.center,
-              maxLines: 2,
-            ),
-            SizedBox(
-              height: 50.h,
-            ),
-            LocaleText(
-              text: LocaleKeys.food_waste_expanded_headline3,
-              style: AppTextStyles.headlineStyle,
-              alignment: TextAlign.center,
-              maxLines: 1,
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            SvgPicture.asset(
-              ImageConstant.FOOD_WASTE_EXPANDED_IMAGE_7,
-              height: 160.81.h,
-            ),
-            SizedBox(
-              height: 20.2.h,
-            ),
-            LocaleText(
-              text: LocaleKeys.food_waste_expanded_body8,
-              style: AppTextStyles.bodyBoldTextStyle,
-              alignment: TextAlign.center,
-            ),
-            SizedBox(
-              height: 40.h,
-            ),
-            CustomButton(
-              width: 372.w,
-              title: LocaleKeys.food_waste_expanded_button,
-              color: AppColors.greenColor,
-              borderColor: AppColors.greenColor,
-              textColor: Colors.white,
-              onPressed: () {
-                Navigator.pushNamed(context, RouteConstant.CUSTOM_SCAFFOLD);
-              },
-            ),
-            SizedBox(
-              height: 30.h,
-            ),
-          ],
-        ),
+              ),
+            ]),
+          ),
+          SizedBox(
+            height: 10.h,
+          ),
+          LocaleText(
+            text: LocaleKeys.food_waste_expanded_body4,
+            style: AppTextStyles.bodyBoldTextStyle,
+            alignment: TextAlign.center,
+            maxLines: 2,
+          ),
+          SizedBox(
+            height: 40.h,
+          ),
+          SvgPicture.asset(
+            ImageConstant.FOOD_WASTE_EXPANDED_IMAGE_4,
+            height: 145.h,
+          ),
+          SizedBox(
+            height: 10.h,
+          ),
+          LocaleText(
+            text: LocaleKeys.food_waste_expanded_body5,
+            style: AppTextStyles.bodyBoldTextStyle,
+            alignment: TextAlign.center,
+            maxLines: 2,
+          ),
+          SizedBox(
+            height: 40.h,
+          ),
+          SvgPicture.asset(
+            ImageConstant.FOOD_WASTE_EXPANDED_IMAGE_5,
+            height: 145.h,
+          ),
+          SizedBox(
+            height: 10.h,
+          ),
+          LocaleText(
+            text: LocaleKeys.food_waste_expanded_body6,
+            style: AppTextStyles.bodyBoldTextStyle,
+            alignment: TextAlign.center,
+            maxLines: 2,
+          ),
+          SizedBox(
+            height: 40.h,
+          ),
+          SvgPicture.asset(
+            ImageConstant.FOOD_WASTE_EXPANDED_IMAGE_6,
+            height: 145.h,
+          ),
+          SizedBox(
+            height: 10.h,
+          ),
+          LocaleText(
+            text: LocaleKeys.food_waste_expanded_body7,
+            style: AppTextStyles.bodyBoldTextStyle,
+            alignment: TextAlign.center,
+            maxLines: 2,
+          ),
+          SizedBox(
+            height: 50.h,
+          ),
+          LocaleText(
+            text: LocaleKeys.food_waste_expanded_headline3,
+            style: AppTextStyles.headlineStyle,
+            alignment: TextAlign.center,
+            maxLines: 1,
+          ),
+          SizedBox(
+            height: 20.h,
+          ),
+          SvgPicture.asset(
+            ImageConstant.FOOD_WASTE_EXPANDED_IMAGE_7,
+            height: 160.81.h,
+          ),
+          SizedBox(
+            height: 20.2.h,
+          ),
+          LocaleText(
+            text: LocaleKeys.food_waste_expanded_body8,
+            style: AppTextStyles.bodyBoldTextStyle,
+            alignment: TextAlign.center,
+          ),
+          SizedBox(
+            height: 40.h,
+          ),
+          CustomButton(
+            width: 372.w,
+            title: LocaleKeys.food_waste_expanded_button,
+            color: AppColors.greenColor,
+            borderColor: AppColors.greenColor,
+            textColor: Colors.white,
+            onPressed: () {
+              Navigator.pushNamed(context, RouteConstant.CUSTOM_SCAFFOLD);
+            },
+          ),
+          SizedBox(
+            height: 30.h,
+          ),
+        ],
       ),
     );
   }
